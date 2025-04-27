dbab084faaadc70dac34d9416de8b3c08f16ab19
Merkle Groot <merklegroot@gmail.com>
2025-04-27 17:06:24 -0400

    analog!

--- roguesrc/build.sh
+++ roguesrc/build.sh
@@ -11,8 +11,11 @@ function build_windows() {
 function build_linux() {
     # Linux outputs here:
     # Rogue/bin/Release/net8.0/linux-x64/publish
+
+    cd Rogue
     dotnet publish -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:EnableCompressionInSingleFile=true
-    echo ./Rouge > Rogue/bin/Release/net8.0/linux-x64/publish/run.sh
+    echo ./Rogue > bin/Release/net8.0/linux-x64/publish/run.sh
+    cd ..
 }
 
 build_linux
